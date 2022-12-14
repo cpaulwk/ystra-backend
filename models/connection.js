50cf8b0444d772338fbcc4b4ba6adc0cec9075b4
Paul Chow <cpaulwenkai@gmail.com>
2022-12-14 14:44:19 +0100

    update connectTimeout to 10000ms

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectionString = process.env.CONNECTION_STRING;
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 mongoose
-  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .connect(connectionString, { connectTimeoutMS: 10000 })
   .then(() => console.log("Database connected"))
   .catch((error) => console.error(error));
