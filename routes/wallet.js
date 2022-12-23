617971dce4d6e64312bf2b247791ec7643e5f273
Boucif <chabani.boucif@gmail.com>
2022-12-23 10:18:29 +0100

    creation fichier

--- routes/wallet.js
+++ routes/wallet.js
@@ -5,6 +5,8 @@ const contract = require("../utils/YSTRAToken.json");
 
 const { EthBlock } = require("../classes/csEtherBlock");
 
+const fs = require('fs');
+
 const contractAddress = process.env.CONTRACT_ADDRESS // '0x41aCbfce44D04D84adfA483FF6b6E8a4Af833319'
 const privateKey=process.env.PRIVATE_KEY_WALLET //"1276a043d16c3945f40201fc0b2d1f448a1d1f1172c02a817bf1b28552fc2488"
 const API_KEY = process.env.API_KEY_ETH //"GHUU2HQ487TJ41WFC9T8FRI3BW8SE3XRN5"
@@ -74,5 +76,40 @@ router.post('/transfert/',async (req, res) => {
     res.status(400).json({ error });
   }
 });
+
+
+router.get ('/testfs',(req,res)=>{
+
+const customer =    {
+        decimalPlaces:0,
+        description:"..and funny.",
+        image:"https://res.cloudinary.com/dlydp2bz3/image/upload/v1671783891/bisumfpzk1ofndfscjgm.png",
+        name:"YSTRA",
+        properties:{
+           category:"Tank",
+           game:"Spider Tanks",
+           rarity:{
+              hexcode:"#369FFF",
+              icon:"https://res.cloudinary.com/dlydp2bz3/image/upload/v1671783891/bisumfpzk1ofndfscjgm.png",
+              label:"Rare",
+              supplyLimit:500
+           },
+           tokenRun:"tank",
+           generation:1
+        },
+        decimals:0,
+     }
+
+const jsonString = JSON.stringify(customer)
+fs.writeFile('./newCustomer.json', jsonString, err => {
+    if (err) {
+        console.log('Error writing file', err)
+        res.json({result:false, err : 'Error writing file'});
+    } else {
+        console.log('Successfully wrote file')
+        res.json({result:true, token : customer});
+    }
+})
+})
 
 module.exports = router;
