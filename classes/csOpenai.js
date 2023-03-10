7eb4897b912ba0c32d123a4cdd0604d58aea10d4
Paul Chow <cpaulwenkai@gmail.com>
2023-03-10 19:45:11 +0100

    update csOpenai file and products with new api key

--- classes/csOpenai.js
+++ classes/csOpenai.js
@@ -1,63 +1,63 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 class ApiOpenai {
-  
-    constructor() {        
-        this.configuration = new Configuration({
-            apiKey:process.env.OPENAI_API_KEY ||'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',
-          });
-    }
-    
-    async generate(inQueryKey,inCountRslt,inResolution ){
+  constructor() {
+    this.configuration = new Configuration({
+      apiKey:
+        // process.env.OPENAI_API_KEY ||
+        // "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",
+        process.env.OPENAI_API_KEY,
+    });
+  }
 
-      if(inQueryKey && inCountRslt>0){
-          const openai = new OpenAIApi(this.configuration);
-          const response = await openai.createImage({
-            prompt: inQueryKey,
-            n: (inCountRslt > 4)? 4 : inCountRslt ,
-            size: this.checkSize(inResolution) ,
-          });
-          this.arrayResult=response.data;
+  async generate(inQueryKey, inCountRslt, inResolution) {
+    if (inQueryKey && inCountRslt > 0) {
+      const openai = new OpenAIApi(this.configuration);
+      const response = await openai.createImage({
+        prompt: inQueryKey,
+        n: inCountRslt > 4 ? 4 : inCountRslt,
+        size: this.checkSize(inResolution),
+      });
+      this.arrayResult = response.data;
+    }
+  }
+
+  // 256x256, 512x512, or 1024x1024
+  checkSize(params) {
+    let tabSize = ["256x256", "512x512", "1024x1024"];
+    if (tabSize.includes(params)) {
+      return params;
+    } else {
+      return "256x256";
+    }
+  }
+
+  async generateWithImage(inQueryKey, url) {
+    try {
+      // This is the Buffer object that contains your image data
+      const buffer = [url];
+      // Set a `name` that ends with .png so that the API knows it's a PNG image
+      buffer.name = "image.png";
+      const openai = new OpenAIApi(this.configuration);
+      const response = await openai.createImageVariation(
+        buffer,
+        1,
+        "1024x1024"
+      );
+      console.log(response);
+      // response.data.data[0].url
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status, error.response.data);
+      } else {
+        console.log(error.message);
       }
     }
+  }
 
-    // 256x256, 512x512, or 1024x1024
-    checkSize (params) {
-      let tabSize=['256x256','512x512','1024x1024'];
-      if(tabSize.includes(params)){
-        return params;
-      }else{
-        return '256x256';
-      }
-    }
-
-    async generateWithImage(inQueryKey,url){
-        try {
-            // This is the Buffer object that contains your image data
-            const buffer = [url];
-            // Set a `name` that ends with .png so that the API knows it's a PNG image
-            buffer.name = "image.png";
-            const openai = new OpenAIApi(this.configuration);
-            const response = await openai.createImageVariation(
-            buffer,
-            1,
-            "1024x1024"
-            );
-            console.log(response);
-            // response.data.data[0].url
-          } catch (error) {
-            if (error.response) {
-              console.log(error.response.status,error.response.data);
-            } else {
-              console.log(error.message);
-            }
-          }
-    }
-
-
-    get Result() {
-        return this.arrayResult;
-    }
+  get Result() {
+    return this.arrayResult;
+  }
 }
 
 module.exports = { ApiOpenai };
--- routes/products.js
+++ routes/products.js
@@ -1,24 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Product = require('../models/products');
-const User = require('../models/users');
+const Product = require("../models/products");
+const User = require("../models/users");
 
-
-router.get('/all/:token',(req,res)=>{
-    User.findOne({token:req.params.token}).then(data=>{
-        if(data){
-            Product.find().then(data=>{
-                res.json({result:true, Products: data})
-            })
-        }else{
-            res.status(401).send({ result: false, error: 'User not found' })            
-        }    
-    }).catch( error=>{
-        console.log(error.message)
-        res.status(500).send({ result: false, error: error.message })
+router.get("/all/:token", (req, res) => {
+  console.log("Reached backend products");
+  User.findOne({ token: req.params.token })
+    .then((data) => {
+      if (data) {
+        Product.find().then((data) => {
+          res.json({ result: true, Products: data });
+        });
+      } else {
+        res.status(401).send({ result: false, error: "User not found" });
+      }
     })
-})
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).send({ result: false, error: error.message });
+    });
+});
 
-
-module.exports=router;
+module.exports = router;
