8d784960da1e4676e234cc6de463af2378738cf8
Paul Chow <cpaulwenkai@gmail.com>
2023-03-24 17:02:15 +0100

    update orders route for new order

--- models/orders.js
+++ models/orders.js
@@ -1,29 +1,32 @@
-const mongoose = require('mongoose');
-const Items = require('./order_items');
-const addressSchema = require('./address');
-const  paymentTypeSchema= require('./payments');
+const mongoose = require("mongoose");
+const Items = require("./order_items");
+const addressSchema = require("./address");
+const paymentTypeSchema = require("./payments");
 
-const statusSchema = new mongoose.Schema({                 
-    status: String,
-    statusDate : Date,
+const statusSchema = new mongoose.Schema({
+  status: String,
+  statusDate: Date,
 });
 
 const orderSchema = mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref:'users'},
-    orderNumber: String, 
-    purchaseDate: Date, 
-    orderItems:{ type: [Items.Schema], required: false },
-    orderStatus: { type: [statusSchema], required: false }, 
-    isCanceled: Boolean,
-    cancelDate: Date,
-    addressBilling:{ type: addressSchema, required: false } ,
-    addressDelivery:{ type: addressSchema, required: false },   /*[id, street, zipCode, city, state, country, phoneNumber, isForBilling, isForDelivery, isDefault, isDeleted,]*/
-    paymentType: { type: paymentTypeSchema, required: false } , /*[payment]*/
-    isPaid: Boolean, 
-    totalPrice: Number,
-    paidDate: Date, 
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  orderNumber: String,
+  purchaseDate: Date,
+  orderItems: { type: [Items.Schema], required: false },
+  orderStatus: { type: [statusSchema], required: false },
+  isCanceled: Boolean,
+  cancelDate: Date,
+  addressBilling: { type: addressSchema, required: false },
+  addressDelivery: {
+    type: addressSchema,
+    required: false,
+  } /*[id, street, zipCode, city, state, country, phoneNumber, isForBilling, isForDelivery, isDefault, isDeleted,]*/,
+  paymentType: { type: paymentTypeSchema, required: false } /*[payment]*/,
+  isPaid: Boolean,
+  totalPrice: Number,
+  paidDate: Date,
 });
 
-const Order = mongoose.model('orders', orderSchema);
+const Order = mongoose.model("orders", orderSchema);
 
 module.exports = Order;
--- routes/orders.js
+++ routes/orders.js
@@ -1,91 +1,97 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const {checkBody} =require('../modules/checkBody');
+const { checkBody } = require("../modules/checkBody");
 const uniqid = require("uniqid");
-const User = require('../models/users');
-const Order= require('../models/orders');
-const Address = require('../models/address');
-const  Payment= require('../models/payments');
+const User = require("../models/users");
+const Order = require("../models/orders");
+// const OrderItem = require("../models/orders_items");
+const Address = require("../models/address");
+const Payment = require("../models/payments");
 
-router.get('/all/:token', (req, res)=>{
-
-  User.findOne({token:req.params.token}).then(data=>{
-    if (data){    
-      Order.find({user:data._id}).sort({purchaseDate:'desc'}).then(data=> {
-        res.json({allOrders: data })
-      });
-    }else{
-      res.status(401).send({ result: false, error: 'User not found' })
-    }
-  }).catch(error=>{
-    res.status(500).send({ result: false, error: error.message })
-  })
-
+router.get("/all/:token", (req, res) => {
+  User.findOne({ token: req.params.token })
+    .then((data) => {
+      if (data) {
+        Order.find({ user: data._id })
+          .sort({ purchaseDate: "desc" })
+          .then((data) => {
+            res.json({ allOrders: data });
+          });
+      } else {
+        res.status(401).send({ result: false, error: "User not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({ result: false, error: error.message });
+    });
 });
 
-router.post ('/new',(req,res)=>{
-  console.log(req.body);
-  const {token, basket,addressBilling,addressDelivery,paymentType,totalPrice } = req.body;
-  User.findOne({token:token}).then(data=>{
-    if(data){
+router.post("/new", (req, res) => {
+  const {
+    token,
+    basket,
+    addressBilling,
+    addressDelivery,
+    paymentType,
+    totalPrice,
+  } = req.body;
+  User.findOne({ token: token })
+    .then((data) => {
+      if (data) {
+        const newPay = {
+          paymentType: "CB",
+          fullName: "Chabani Boucif",
+          expirationDate: "15/03/2022",
+          isDefault: true,
+        };
 
-      console.log("token",token);
-      const newPay=  {
-        paymentType: 'CB',
-        fullName: 'Chabani Boucif', 
-        expirationDate: '15/03/2022', 
-        isDefault: true, 
-      };
+        const newAddress = {
+          addressName: addressDelivery?.addressName || "",
+          street: addressDelivery?.addressStreet || "",
+          zipCode: addressDelivery?.zipCode || "",
+          city: addressDelivery?.city || "",
+          state: addressDelivery?.state || "",
+          country: addressDelivery?.country || "",
+          phoneNumber: addressDelivery?.phoneNumber || "",
+          isForBilling: addressDelivery?.isForBilling || false,
+          isForDelivery: addressDelivery?.isForDelivery || false,
+          isDefault: addressDelivery?.isDefault || false,
+          isDeleted: addressDelivery?.isDeleted || false,
+        };
 
-      const newAddress={
-        addressName: addressDelivery?.addressName || '',
-        street: addressDelivery?.street || '', 
-        zipCode:addressDelivery?.zipCode || '', 
-        city: addressDelivery?.city || '', 
-        state: addressDelivery?.state || '',
-        country: addressDelivery?.country || '', 
-        phoneNumber: addressDelivery?.phoneNumber || '',
-        isForBilling: addressDelivery?.isForBilling || false,
-        isForDelivery: addressDelivery?.isForDelivery || false,
-        isDefault: addressDelivery?.isDefault || false,
-        isDeleted: addressDelivery?.isDeleted || false,
-      };
-      
-      const newOrder=new Order({
-        user: data._id,
-        orderNumber: `OD${uniqid()}`, 
-        purchaseDate: Date.now(), 
-        orderStatus: {               
-                    status: 'Order',
-                    statusDate : Date.now(),
-                  }, 
-        isCanceled: false,
-        cancelDate: null,
-        addressBilling:newAddress ,
-        addressDelivery:newAddress,   /*[id, street, zipCode, city, state, country, phoneNumber, isForBilling, isForDelivery, isDefault, isDeleted,]*/
-        paymentType: newPay , /*[payment]*/
-        isPaid: true, 
-        totalPrice:totalPrice,
-        paidDate: Date.now(), 
+        const newOrder = new Order({
+          user: data._id,
+          orderNumber: `OD${uniqid()}`,
+          purchaseDate: Date.now(),
+          orderStatus: {
+            status: "Order",
+            statusDate: Date.now(),
+          },
+          isCanceled: false,
+          cancelDate: null,
+          addressBilling: newAddress,
+          addressDelivery:
+            newAddress /*[id, street, zipCode, city, state, country, phoneNumber, isForBilling, isForDelivery, isDefault, isDeleted,]*/,
+          paymentType: newPay /*[payment]*/,
+          isPaid: true,
+          totalPrice: totalPrice,
+          paidDate: Date.now(),
+        });
 
-      })
+        //   basket.forEach((element) => {
+        //     newOrder.orderItems.push(element);
+        //   });
 
-      basket.forEach(element => {
-        newOrder.orderItems.push(element);
-      });
- 
-      newOrder.save().then((newDoc)=>{
-        res.json({result:true, Order : newDoc});
-      })
-
-    }else{
-        res.status(401).send({ result: false, error: 'User not found' })
-    }
-  }).catch(error=>{
-    res.status(500).send({ result: false, error: error.message })
-  })
-
-
+        newOrder.save().then((newDoc) => {
+          res.json({ result: true, Order: newDoc });
+        });
+      } else {
+        res.status(401).send({ result: false, error: "User not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({ result: false, error: error.message });
+    });
 });
 
-module.exports=router;
+module.exports = router;
